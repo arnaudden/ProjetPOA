b4b3b83a50ee87020adb85ed0d9a49942014c72c
arnaudden <arnaud.denizet@gmail.com>
2016-04-14 22:22:51 +0200

    Classes Utilisateur et Facture OK

---
+++ app/src/main/java/projet/poa/uqac/colocmanager/Activities/UtilisateurActivity.java
@@ -0,0 +1,17 @@
+package projet.poa.uqac.colocmanager.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import projet.poa.uqac.colocmanager.R;
+
+public class UtilisateurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utilisateur);
+
+
+    }
+}
--- app/src/main/java/projet/poa/uqac/colocmanager/Facture.java
+++ app/src/main/java/projet/poa/uqac/colocmanager/Facture.java
@@ -1,12 +1,85 @@
 package projet.poa.uqac.colocmanager;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 /**
  * Cette classe gère les factures rentrées par l'utilisateur
  * Created by Arnaud
  */
 public class Facture {
 
+    private GregorianCalendar dateAchat;
 
+    private String titre;
+
+    private String magasinAchat;
+
+    private String description;
+
+    private ArrayList<Utilisateur> listePersonneIntervenant;
+
+    private double coutTotal;
+
+    private double coutParPersonne;
+
+    private boolean factureReglee;
+
+
+    public Facture(GregorianCalendar date,
+                   String t, String magasin,
+                   String descr, ArrayList<Utilisateur> listPerson,
+                   double coutTot)
+    {
+
+        dateAchat = date;
+
+        titre = t;
+
+        magasinAchat = magasin;
+
+        description = descr;
+
+        listePersonneIntervenant = listPerson;
+
+        coutTotal = coutTot;
+
+        coutParPersonne = coutTot/listePersonneIntervenant.size();
+
+        factureReglee = false;
+    }
+
+    public GregorianCalendar getDateAchat() {
+        return dateAchat;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getMagasinAchat() {
+        return magasinAchat;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ArrayList<Utilisateur> getListePersonneIntervenant() {
+        return listePersonneIntervenant;
+    }
+
+    public double getCoutTotal() {
+        return coutTotal;
+    }
+
+    public double getCoutParPersonne() {
+        return coutParPersonne;
+    }
+
+    public boolean isFactureReglee() {
+        return factureReglee;
+    }
 
 
 }
